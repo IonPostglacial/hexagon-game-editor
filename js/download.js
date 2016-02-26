aa2403b6a2a667dd5be2aaa1fa0848ea7205ecd4
Nicolas Galipot <nicolas.galipot@gmail.com>
2016-02-26 21:43:54 +0100

    Addition of buttons to download / upload.

--- js/download.js
+++ js/download.js
@@ -1,20 +1,20 @@
-(() => {
-"use strict";
+function initDownload(grid, uploaded, uploadScene, downloadScene) {
+  "use strict";
 
-window.addEventListener('load', (e) => {
-const $ = document.querySelector.bind(document);
-  $('#uploaded').onchange = (e) => {
+  uploadScene.onclick = (e) => {
+    uploaded.click();
+  };
+
+  uploaded.onchange = (e) => {
     const READER = new FileReader();
     READER.onload = (e) => {
-      window.obstacles = JSON.parse(READER.result);
+      grid.obstacles = JSON.parse(READER.result);
     };
-    READER.readAsText($('#uploaded').files[0]);
+    READER.readAsText(uploaded.files[0]);
   };
 
-  $('#download-scene').onclick = (e) => {
-    var blob = new Blob([JSON.stringify(window.obstacles)], {type : 'application/json'});
-    $('#download-scene').href = URL.createObjectURL(blob);
+  downloadScene.onclick = (e) => {
+    var blob = new Blob([JSON.stringify(grid.obstacles)], {type : 'application/json'});
+    downloadScene.href = URL.createObjectURL(blob);
   };
-});
-return {};
-})();
+}
--- js/mapeditor.js
+++ js/mapeditor.js
@@ -1,46 +1,39 @@
-(() => {
-"use strict";
+function initMapEditor(grid, width, height, scene, coordMouse, coordHex) {
+  "use strict";
 
-const SCENE_WIDTH = 800;
-const SCENE_HEIGHT = 600;
-const GRID = {width: 14, height:6, radius: 32};
-window.obstacles = [{x: 5, y: 5}];
-
-window.addEventListener('load', (e) => {
-const $ = document.querySelector.bind(document);
-  const CTX = Object.assign($('#scene'), {width: SCENE_WIDTH, height: SCENE_HEIGHT}).getContext("2d");
+  const CTX = Object.assign(scene, {width: width, height: height}).getContext("2d");
   let firstStep = {x: 0, y: 0};
   let lastStep = {x: 0, y: 0};
   let path = [];
 
   function drawScene() {
     CTX.fillStyle = "rgb(0, 0, 0)";
-    CTX.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
-    hexagon.grid.draw(CTX, GRID, "rgb(0, 0, 255)");
-    for (let obstacle of window.obstacles) {
-      const obstacleCoord = hexagon.grid.axisToPixel(GRID, obstacle.x, obstacle.y);
-      hexagon.draw(CTX, obstacleCoord, GRID.radius, "rgb(255, 255, 0)");
+    CTX.fillRect(0, 0, width, height);
+    hexagon.grid.draw(CTX, grid, "rgb(0, 0, 255)");
+    for (let obstacle of grid.obstacles) {
+      const obstacleCoord = hexagon.grid.axisToPixel(grid, obstacle.x, obstacle.y);
+      hexagon.draw(CTX, obstacleCoord, grid.radius, "rgb(255, 255, 0)");
     }
     for (let step of path) {
-      const stepCoord = hexagon.grid.axisToPixel(GRID, step.x, step.y);
-      hexagon.draw(CTX, stepCoord, GRID.radius, "rgb(0, 255, 0)");
+      const stepCoord = hexagon.grid.axisToPixel(grid, step.x, step.y);
+      hexagon.draw(CTX, stepCoord, grid.radius, "rgb(0, 255, 0)");
     }
-    hexagon.draw(CTX, hexagon.grid.axisToPixel(GRID, firstStep.x, firstStep.y), GRID.radius, "rgb(255, 0, 0)");
-    hexagon.draw(CTX, hexagon.grid.axisToPixel(GRID, lastStep.x, lastStep.y), GRID.radius, "rgb(255, 0, 0)");
+    hexagon.draw(CTX, hexagon.grid.axisToPixel(grid, firstStep.x, firstStep.y), grid.radius, "rgb(255, 0, 0)");
+    hexagon.draw(CTX, hexagon.grid.axisToPixel(grid, lastStep.x, lastStep.y), grid.radius, "rgb(255, 0, 0)");
   }
 
   drawScene();
 
-  $('#scene').onmousemove = (e) => {
-    const currentCoordinates = hexagon.grid.pixelToAxis(GRID, e.offsetX, e.offsetY);
+  scene.onmousemove = (e) => {
+    const currentCoordinates = hexagon.grid.pixelToAxis(grid, e.offsetX, e.offsetY);
     if (lastStep.x !== currentCoordinates.x || lastStep. y !== currentCoordinates.y) {
       lastStep = currentCoordinates;
       path = PF.shortestPathBetween(firstStep, lastStep, PF.hexDistance, (pos) => {
-        const maxDimension = Math.max(GRID.width, GRID.height * 2 - 1);
-        if (pos.x < -maxDimension || pos.x > maxDimension || pos.y < 0 || pos.y > GRID.height * 2 - 1) {
+        const maxDimension = Math.max(grid.width, grid.height * 2 - 1);
+        if (pos.x < -maxDimension || pos.x > maxDimension || pos.y < 0 || pos.y > grid.height * 2 - 1) {
           return false;
         }
-        for (let obstacle of window.obstacles) {
+        for (let obstacle of grid.obstacles) {
           if (pos.x === obstacle.x && pos.y === obstacle.y) {
             return false;
           }
@@ -49,15 +42,14 @@ const $ = document.querySelector.bind(document);
       });
     }
     drawScene();
-    $('#coord-mouse').rows[0].cells[1].innerHTML = e.offsetX;
-    $('#coord-mouse').rows[0].cells[3].innerHTML = e.offsetY;
-    $('#coord-hex').rows[0].cells[1].innerHTML = lastStep.x;
-    $('#coord-hex').rows[0].cells[3].innerHTML = lastStep.y;
+    coordMouse.rows[0].cells[1].innerHTML = e.offsetX;
+    coordMouse.rows[0].cells[3].innerHTML = e.offsetY;
+    coordHex.rows[0].cells[1].innerHTML = lastStep.x;
+    coordHex.rows[0].cells[3].innerHTML = lastStep.y;
   };
 
-  $('#scene').onclick = (e) => {
-    const obstacle = hexagon.grid.pixelToAxis(GRID, e.offsetX, e.offsetY);
-    window.obstacles.push(obstacle);
+  scene.onclick = (e) => {
+    const obstacle = hexagon.grid.pixelToAxis(grid, e.offsetX, e.offsetY);
+    grid.obstacles.push(obstacle);
   };
-});
-})();
+}
