04accce2b7ffae4e9685b79e5e1ca18168090232
Nicolas Galipot <nicolas.galipot@gmail.com>
2016-03-10 00:13:58 +0100

    Add the possibility to choose between multiple tile types.

--- js/mapeditor.js
+++ js/mapeditor.js
@@ -1,10 +1,17 @@
 define(["lib/hexagon", "lib/pathfinding"], (hexagon, PF) => {
-  return { init(grid, layers, coordMouse, coordHex) {
+  return { init(grid, layers, tileTypeSelector, coordMouse, coordHex) {
     "use strict";
 
     const SCENE_WIDTH = hexagon.grid.pixelWidth(grid);
     const SCENE_HEIGHT = hexagon.grid.pixelHeight(grid);
-    const TILE_OBSTACLE = 1;
+    const TileType = { LAND: 0, ROCK: 1, WATER: 2, VOID: 3 };
+    const TileProperties = [
+      {obstacle: false, color: "rgb(100, 50, 0)"},
+      {obstacle: true, color: "rgb(100, 100, 100)"},
+      {obstacle: true, color: "rgb(0, 150, 200)"},
+      {obstacle: true, color: "rgb(0, 0, 0)"}
+    ];
+    let selectedTileType = TileType.LAND;
 
     layers.style.width = SCENE_WIDTH + "px";
     layers.style.height = SCENE_HEIGHT + "px";
@@ -18,17 +25,17 @@ define(["lib/hexagon", "lib/pathfinding"], (hexagon, PF) => {
     let lastStep = {x: 0, y: 0};
     let path = [];
 
-    BG_CTX.fillStyle = "rgb(0, 0, 0)";
+    BG_CTX.fillStyle = "rgb(50, 50, 50)";
     BG_CTX.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
-    hexagon.grid.draw(BG_CTX, grid, "rgb(0, 0, 255)");
+    hexagon.grid.draw(BG_CTX, grid, "rgb(150, 150, 150)");
 
     function drawScene() {
       CTX.clearRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
       for (let entry of grid.data) {
         let pos = entry[0], tile = entry[1];
-        if (tile === TILE_OBSTACLE) {
+        if (tile !== null) {
           const obstacleCoord = hexagon.grid.axisToPixel(grid, pos.x, pos.y);
-          hexagon.draw(CTX, obstacleCoord, grid.radius, "rgb(255, 255, 0)");
+          hexagon.draw(CTX, obstacleCoord, grid.radius, TileProperties[tile].color);
         }
       }
       for (let step of path) {
@@ -49,7 +56,7 @@ define(["lib/hexagon", "lib/pathfinding"], (hexagon, PF) => {
           if (!hexagon.grid.contains(grid, pos.x, pos.y)) {
             return false;
           }
-          return grid.data.get(pos) !== TILE_OBSTACLE;
+          return grid.data.get(pos) === null || !TileProperties[grid.data.get(pos)].obstacle;
         });
         drawScene();
       }
@@ -59,10 +66,14 @@ define(["lib/hexagon", "lib/pathfinding"], (hexagon, PF) => {
       coordHex.rows[0].cells[3].innerHTML = lastStep.y;
     };
 
+    tileTypeSelector.onclick = (e) => {
+      selectedTileType = parseInt(tileTypeSelector.elements['tile-type'].value);
+    };
+
     layers.onclick = (e) => {
       const obstacle = hexagon.grid.pixelToAxis(grid, e.offsetX, e.offsetY);
       if(hexagon.grid.contains(grid, obstacle.x, obstacle.y)) {
-        grid.data.set(obstacle, TILE_OBSTACLE);
+        grid.data.set(obstacle, selectedTileType);
       }
     };
   }}
