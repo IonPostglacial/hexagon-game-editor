4dc1518354f2fa37d8a25cfdcbda75065965cb41
Nicolas Galipot <nicolas.galipot@gmail.com>
2016-04-29 01:19:29 +0200

    Remove compilation steps for now since most browsers already support es2015 features used in hexagon and it lengthens the feedback loop in dev.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,18 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
-const buffer = require('vinyl-buffer');
 const source = require('vinyl-source-stream');
-const sourcemaps = require('gulp-sourcemaps');
 const http = require('http');
 const st = require('st')
 const livereload = require('gulp-livereload');
-const es = require('event-stream');
-const path = require('path');
 
 gulp.task('js', function() {
     return browserify({
       debug: true,
       entries: ['js/main.js'],
     })
-    .transform('babelify', { presets: ['es2015', 'react'] })
     .bundle()
     .on('error', function (err) { console.error(err); })
-    .pipe(source(path.basename('js/main.js')))
-    //.pipe(sourcemaps.init({ loadMaps: true }))
-    //.pipe(sourcemaps.write())
+    .pipe(source('main.js'))
     .pipe(gulp.dest('dist'));
 });
 
@@ -28,5 +21,5 @@ gulp.task('default', ['js'], function (done) {
     st({ index: 'hex.html', cache: false, path: './' })
 	).listen(8080, done);
 	livereload.listen();
-	gulp.watch('src/*', ['js']);
+	gulp.watch('js/*', ['js']);
 });
